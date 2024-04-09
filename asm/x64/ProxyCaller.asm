044f0c5e0a9eb89bb7bc5a8dd5d36655e0328633
coremedic <ben@aptlabs.net>
2024-04-09 13:06:39 -0400

    restructure

--- asm/x64/ProxyCaller.asm
+++ asm/x64/ProxyCaller.asm
@@ -5,7 +5,14 @@ option win64:1
 
 public WorkCallback
 
+.data
+
+wSyscallServiceNumber       DWORD   0
+qSyscallInsAddress QWORD   0
+
 .code
+
+SetSyscallServiceNumber
 
 WorkCallback PROC
     mov rbx, rdx
@@ -20,5 +27,18 @@ WorkCallback PROC
     mov [rsp + 028h], r10
     jmp rax
 WorkCallback ENDP
+
+ProxyIndirect PROC
+    mov rbx, rdx
+    mov rax, [rbx]
+    mov rcx, [rbx + 08h]
+    mov rdx, [rbx + 010h]
+    xor r8,  r8
+    mov r9,  [rbx + 018h]
+    mov r10, [rbx + 020h]
+    mov [rsp + 030h], r10
+    mov r10, 03000h
+    mov [rsp + 028h], r10
+
 
 end
